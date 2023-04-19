70ccd2af2f9c859c7f7ef1dfafc2c9f80cea0108
C57c4r1t1 <dangviluanwk@gmail.com>
2023-04-19 18:21:32 +0700

    add

--- C# and MVC design pattern/TestProject/Program.cs
+++ C# and MVC design pattern/TestProject/Program.cs
@@ -4,40 +4,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Globalization;
-
+using System.Runtime.Remoting.Services;
+using System.Threading;
+using System.Security.Cryptography;
+using System.Reflection;
 
 namespace TestProject
 {
+    class Program {
 
-
-    class Program
-    {
         static void Main(string[] args)
         {
-            CultureInfo cultureInfo = new CultureInfo("en-US");
-            Console.Write("The table number: ");
-            string table = Console.ReadLine();
-            Console.Write("The number of people: ");
-            string countString = Console.ReadLine();
-            int.TryParse(countString, out int count);
-            Console.Write("The reservation date (MM/dd/yyyy): ");
-            string dateTimeString = Console.ReadLine();
-            if (!DateTime.TryParseExact(
-            dateTimeString,
-            "M/d/yyyy HH:mm",
-            cultureInfo,
-            DateTimeStyles.None,
-            out DateTime dateTime))
-            {
-                dateTime = DateTime.Now;
-            }
-            Console.WriteLine(
-            "Table {0} has been booked for {1} people on {2}at { 3}",
-table,
-count,
-dateTime.ToString("M/d/yyyy", cultureInfo),
-dateTime.ToString("HH:mm", cultureInfo));
+            //Get user input for length and width
+            Console.Write("Enter the width and length of the terrain: ");
+            string input = Console.ReadLine();
+            string[] values = input.Split(' ');
+            int row = int.Parse(values[0]);
+            int col = int.Parse(values[1]);
 
+
+            //Prompt
+            Console.WriteLine("Populating the Terrain");
+            Thread.Sleep(1000);
+
+            //Randomly populate the map
+            TerrainEnum[,] map = new TerrainEnum[row, col];
+            for (int i = 0; i < row * col; i++)
+            {
+                TerrainEnum random = new TerrainEnum();
+                int RowIndex = i / col;
+                int ColIndex = i % col;
+                map[RowIndex, ColIndex] = random.GetRandomTerrain();
+            }
+
+            Console.OutputEncoding = UTF8Encoding.UTF8;
+            
+            for(int i = 0; i < (map.GetLength(0) * map.GetLength(1)); i++)
+            {
+                //Initialize variable
+                int RowIndex = i / map.GetLength(1);
+                int ColIndex = i % map.GetLength(1);
+                TerrainEnum position = map[RowIndex, ColIndex];
+                bool checkEndOfLine = (i + 1) % map.GetLength(1) == 0;
+
+                //Add char and color
+                Console.ForegroundColor = position.GetColor();
+                Console.Write(position.GetChar() + (checkEndOfLine ? "\n" : " "));
+            }
+            
         }
     }
 }
---
+++ C# and MVC design pattern/TestProject/TerrainEnum.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    public enum TerrainEnum
+    {
+        GRASS,
+        SAND,
+        WATER,
+        WALL
+    }
+    public static class TerrainEnumExtension
+    {
+        public static ConsoleColor GetColor(this TerrainEnum terrain)
+        {
+            switch (terrain)
+            {
+                case TerrainEnum.GRASS: return ConsoleColor.Green;
+                case TerrainEnum.SAND: return ConsoleColor.Yellow;
+                case TerrainEnum.WATER: return ConsoleColor.Blue;
+                default: return ConsoleColor.DarkGray;
+            }
+        }
+
+        public static char GetChar(this TerrainEnum terrain)
+        {
+            switch (terrain)
+            {
+                case TerrainEnum.GRASS: return '\u201c';
+                case TerrainEnum.SAND: return '\u25cb';
+                case TerrainEnum.WATER: return '\u2248';
+                default: return '\u25cf';
+            }
+        }
+
+        private static Random random = new Random();
+
+        public static TerrainEnum GetRandomTerrain(this TerrainEnum terrainEnum)
+        {
+            //Retrieve all possible enum values in TerrainEnum
+            var values = Enum.GetValues(typeof(TerrainEnum));
+            //Get a random Terrain
+            TerrainEnum RandomTerrain = (TerrainEnum)values.GetValue(random.Next(values.Length));
+
+            return RandomTerrain;
+        }
+
+    }
+}
